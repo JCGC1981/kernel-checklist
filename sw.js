10f933cb8bd1661e648d9b281aba7524333eb385
JCGC1981 <julboherces@gmail.com>
2026-09-20 17:59:49 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE = 'kernel-checklist-v1';
+const FILES = ['./', './index.html', './logo.jpg', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request)
+      .then(r => { const copy = r.clone(); caches.open(CACHE).then(c => c.put(e.request, copy)); return r; })
+      .catch(() => caches.match(e.request))
+  );
+});
